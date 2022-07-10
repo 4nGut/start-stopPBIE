2e0e8994351b92c6703f13c2e8f1fee34e47d256
Antonio Gutiérrez <43992692+4nGut@users.noreply.github.com>
2022-07-10 03:52:03 -0500

    Add files via upload

---
+++ Start-PBIE.ps1
@@ -0,0 +1,42 @@
+<#
+    .DESCRIPTION
+        This script starts a Power BI Embedded resource using PowerShell 5.1 and an Azure Automation Account,
+        before using it the modules Az.Accounts and Az.PowerBIEmbedded should be updated, as well as
+        generate the 4 variables used below, and assign the appropiate permissions to the Automation Account
+        System Generated Identity
+
+    .NOTES
+        AUTHOR: Antonio Gutiérrez 4nGut
+        LASTEDIT: Jul 10, 2022
+        CC0 1.0 Universal (CC0 1.0)
+        Public Domain Dedication
+#>
+
+"Please enable appropriate RBAC permissions to the system identity of this automation account. Otherwise, the runbook may fail..."
+
+$tid = Get-AutomationVariable -Name 'TenantID'
+$sid = Get-AutomationVariable -Name 'SubscriptionID'
+$pbie = Get-AutomationVariable -Name 'pbie'
+$pbierg = Get-AutomationVariable -Name 'pbierg'
+
+try
+{
+    "Logging in to Azure..."
+    Connect-AzAccount -Tenant $tid -Subscription $sid -Identity
+}
+catch {
+    Write-Error -Message $_.Exception
+    throw $_.Exception
+}
+
+
+try
+{
+	Resume-AzPowerBIEmbeddedCapacity -Name $pbie -ResourceGroupName $pbierg -PassThru
+	Write-Output "Power BI Embedded Service Successfully Started"
+}
+catch
+{
+	Write-Error -Message $_.Exception
+	throw  $_.Exception
+}
